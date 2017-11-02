02225b110c3421cbeb0554a6270493be9d526364
shenshuiping <shenshuiping@jasgroup.cn>
2017-11-02 13:23:56 +0800

    regist some property for nativeView

--- src/amap.android.ts
+++ src/amap.android.ts
@@ -16,10 +16,36 @@ export class AMapView extends AMapViewBase {
 
     initMap(): void {
         if(this.mapView) return;
-        let mapOptions = new com.amap.api.maps2d.AMapOptions();
+        let mapOptions = this.mapOptions();
         this.mapView = new com.amap.api.maps2d.MapView(this._context, mapOptions);
         this.mapView.onCreate(null);
         this.nativeView.addView(this.mapView);
+    }
+
+    mapOptions(): any {
+        let mapOptions = new com.amap.api.maps2d.AMapOptions();
+        if(this.config.logoPosition !== undefined) {
+            mapOptions.logoPosition(this.config.logoPosition);
+        }
+        if(this.config.mapType !== undefined) {
+            mapOptions.mapType(this.config.mapType);
+        }
+        if(this.config.scaleControlsEnabled !== undefined) {
+            mapOptions.scaleControlsEnabled(this.config.scaleControlsEnabled);
+        }
+        if(this.config.scrollGesturesEnabled !== undefined) {
+            mapOptions.scrollGesturesEnabled(this.config.scrollGesturesEnabled);
+        }
+        if(this.config.zoomControlsEnabled !== undefined) {
+            mapOptions.zoomControlsEnabled(this.config.zoomControlsEnabled);
+        }
+        if(this.config.zoomGesturesEnabled !== undefined) {
+            mapOptions.zoomGesturesEnabled(this.config.zoomGesturesEnabled);
+        }
+        if(this.config.zOrderOnTop !== undefined) {
+            mapOptions.zOrderOnTop(this.config.zOrderOnTop);
+        }
+        return mapOptions;
     }
 
     getMapView(): any {
--- src/amap.common.ts
+++ src/amap.common.ts
@@ -1,4 +1,4 @@
-import { View } from "tns-core-modules/ui/core/view";
+import { View, Property, booleanConverter } from "tns-core-modules/ui/core/view";
 
 export interface AMapViewApi {
 
@@ -8,26 +8,81 @@ export abstract class AMapViewCommonBase extends View implements AMapViewApi {
   
 }
 
+// 设置Logo位置
+export const logoPositionProperty = new Property<AMapViewCommonBase, number>({ name: "logoPosition" });
+logoPositionProperty.register(AMapViewCommonBase);
+
+// 设置一个地图显示类型以改变初始的类型。
+export const mapTypeProperty = new Property<AMapViewCommonBase, number>({ name: "mapType" });
+mapTypeProperty.register(AMapViewCommonBase);
+
+// 设置比例尺功能是否可用
+export const scaleControlsEnabledProperty = new Property<AMapViewCommonBase, boolean>({ name: "	scaleControlsEnabled" });
+scaleControlsEnabledProperty.register(AMapViewCommonBase);
+
+// 设置指南针是否可用。
+export const scrollGesturesEnabledProperty = new Property<AMapViewCommonBase, boolean>({ name: "scrollGesturesEnabled" });
+scrollGesturesEnabledProperty.register(AMapViewCommonBase);
+
+// 设置地图是否允许缩放。
+export const zoomControlsEnabledProperty = new Property<AMapViewCommonBase, boolean>({ name: "zoomControlsEnabled" });
+zoomControlsEnabledProperty.register(AMapViewCommonBase);
+
+// 设置地图是否可以通过手势进行缩放。
+export const zoomGesturesEnabledProperty = new Property<AMapViewCommonBase, boolean>({ name: "zoomGesturesEnabled" });
+zoomGesturesEnabledProperty.register(AMapViewCommonBase);
+
+// 设置Z轴排序是否被允许。
+export const 	zOrderOnTopProperty = new Property<AMapViewCommonBase, boolean>({ name: "zOrderOnTop" });
+zOrderOnTopProperty.register(AMapViewCommonBase);
+
+
 export abstract class AMapViewBase extends AMapViewCommonBase {
+  
+    protected config: any = {};
+  
+    get ios(): any {
+      return this.nativeView;
+    }
+  
+    set ios(value) {
+      this.nativeView = value;
+    }
+  
+    get android(): any {
+      return this.nativeView;
+    }
+  
+    set android(value) {
+      this.nativeView = value;
+    }
 
-  protected config: any = {};
+    [logoPositionProperty.setNative](value: number) {
+      this.config.logoPosition = value;
+    }
 
-  get ios(): any {
-    return this.nativeView;
+    [mapTypeProperty.setNative](value: number) {
+      this.config.mapType = value;
+    }
+
+    [scaleControlsEnabledProperty.setNative](value: boolean) {
+      this.config.scaleControlsEnabled = value;
+    }
+
+    [scrollGesturesEnabledProperty.setNative](value: boolean) {
+      this.config.scrollGesturesEnabled = value;
+    }
+
+    [zoomControlsEnabledProperty.setNative](value: boolean) {
+      this.config.zoomControlsEnabled = value;
+    }
+
+    [zoomGesturesEnabledProperty.setNative](value: boolean) {
+      this.config.zoomGesturesEnabled = value;
+    }
+
+    [zOrderOnTopProperty.setNative](value: boolean) {
+      this.config.zOrderOnTop = value;
+    }
+  
   }
-
-  set ios(value) {
-    this.nativeView = value;
-  }
-
-  get android(): any {
-    return this.nativeView;
-  }
-
-  set android(value) {
-    this.nativeView = value;
-  }
-
-}
-
-
